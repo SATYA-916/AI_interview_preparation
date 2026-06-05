27fa47b16a24a257704a382336b4a6afb5a1c64f
SATYA-916 <satyasampath79@gmail.com>
2026-06-05 23:34:28 +0530

    Allow Vercel origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,14 +6,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://ai-interview-preparation-dun.vercel.app"
-    ],
+    origin: true,
     credentials: true
 }))
-
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
